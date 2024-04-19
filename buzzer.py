257d0324246a5f26574819d2cb87e94a3ce97a6e
berzi <anon>
2024-04-20 01:03:07 +0530

    added buzzercode

--- buzzer.py
+++ buzzer.py
@@ -1,11 +1,24 @@
 import os
+import RPi.GPIO as GPIO 
+import time 
+
 def buzzer():
     with open('redlist.txt','r') as f:
         x = f.read() 
         if os.path.getsize('./buzzer.py') == 0:
             print("empty")
         else:
-            print("intruder detected")
+            #buzzer code
+            GPIO.setmode(GPIO.BOARD)
+            GPIO.setup(8,GPIO.OUT)
+            GPIO.output(8,True)
+            #print("buzzer is on")
+            time.sleep(1)
+            GPIO.OUTPUT(8,False)
+            #print(:buzzer is off")
+            #time.sleep(1)
+            #buzzer code
+            #print("intruder detected")
             f.close()
             with open('redlist.txt','w'):
                 pass
